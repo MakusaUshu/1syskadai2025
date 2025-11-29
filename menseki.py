46821f61e551fee1996e7f15241801a59a51f423
Makusa Ushu <zuojiujianxiu3@gmail.com>
2025-11-29 20:48:21 +0900

    Add a command

--- menseki.py
+++ menseki.py
@@ -9,7 +9,12 @@ a = float(sys.argv[2])
 n = float(sys.argv[3]) 
 
 
-if sys.argv[1] == "L":
+if n <= 2:
+
+    print("最後の文字は3以上の整数にしてください")
+
+
+elif sys.argv[1] == "L":
     
     print( ( n * ( a * a ) ) / ( 4 * math.tan( math.pi / n ) ) ) 
 
