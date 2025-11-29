13281acd15bf59ad999d21a68db40237325037f8
Makusa Ushu <zuojiujianxiu3@gmail.com>
2025-11-29 22:18:22 +0900

    Add a command

--- menseki.py
+++ menseki.py
@@ -5,28 +5,30 @@
 import math 
 import sys 
 
-a = float(sys.argv[2]) 
-n = float(sys.argv[3]) 
+
+try:
+    a = float(sys.argv[2]) 
+    n = float(sys.argv[3])
+
+except ValueError:
+    print("2番目、3番目の文字は数字にしてください")
 
 
 if not n.is_integer() or n <= 2:
-
     print("最後の文字は3以上の整数にしてください")
 
 
 elif sys.argv[1] == "L":
-    
     print( ( n * ( a * a ) ) / ( 4 * math.tan( math.pi / n ) ) ) 
 
 
 elif sys.argv[1] == "R": 
-
     print( ( 1 / 2 ) * a * a * math.sin( ( 2 * math.pi ) / n ) * n ) 
 
 
-elif not sys.argv[1] == "L" or sys.argv[1] == "R":
-    
+elif not sys.argv[1] == "L" or sys.argv[1] == "R":    
     print("最初の文字はLまたはRを入力してください")
 
+
 else: 
-    print("2番目、3番目の文字は数字にしてください")
+    print("error")
